d02a7ce0adf3e5ebd59d2a0091a80a1ae07d8abe
JarkkoLinnanvirta <posti@taitavasti.fi>
2014-02-14 17:59:00 +0200

    Create fi_FI.php
    Finnish translation.

---
+++ lang/fi_FI.php
@@ -0,0 +1,10 @@
+<?php
+
+global $lang;
+
+
+// Output for class or file: InvisibleSpamProtectionField
+$lang['en_US']['InvisibleSpamProtectionField']['LEAVEEMPTY'] = 
+	'Jos näet tämän kentän, jätä se tyhjäksi. Kenttää käytetään automaattisten roskapostittajien estämiseksi.';
+$lang['en_US']['InvisibleSpamProtectionField']['FIELDNOTEMPTY'] = 
+	'Roskapostisuoja-kenttä ei ole tyhjä. Kyseinen kenttä pitää jättää tyhjäksi.';
