79171e5ae9eee77b16e25b20d6602c4d430a4fd9
helpfulrobot <chris@silverstripe.com>
2015-12-18 07:57:22 +1300

    Converted to PSR-2

--- code/InvisibleSpamProtector.php
+++ code/InvisibleSpamProtector.php
@@ -4,29 +4,33 @@
  * @package InvisibleSpamProtection
  */
 
-class InvisibleSpamProtector implements SpamProtector {
-	/**
-	 * Returns the {@link InvisibleSpamProtectorField} associated with this protector
-	 *
-	 * @return InvisibleSpamProtectorField
-	 */
-	public function getFormField($name = null, $title = null, $value = null, $form = null, $rightTitle = null) {
-		return new InvisibleSpamProtectorField($name, $title, $value, $form, $rightTitle);
-	}
+class InvisibleSpamProtector implements SpamProtector
+{
+    /**
+     * Returns the {@link InvisibleSpamProtectorField} associated with this protector
+     *
+     * @return InvisibleSpamProtectorField
+     */
+    public function getFormField($name = null, $title = null, $value = null, $form = null, $rightTitle = null)
+    {
+        return new InvisibleSpamProtectorField($name, $title, $value, $form, $rightTitle);
+    }
 
-	/**
-	 * Function required to handle dynamic feedback of the system.
-	 * if unneeded just return true
-	 *
-	 * @return true
-	 */
-	public function sendFeedback($object = null, $feedback = "") {
-		return true;
-	}
+    /**
+     * Function required to handle dynamic feedback of the system.
+     * if unneeded just return true
+     *
+     * @return true
+     */
+    public function sendFeedback($object = null, $feedback = "")
+    {
+        return true;
+    }
 
-	/**
-	 * Not used by InvisibleSpamProtection
-	 */
-	public function setFieldMapping($fieldMapping) {}
-	
+    /**
+     * Not used by InvisibleSpamProtection
+     */
+    public function setFieldMapping($fieldMapping)
+    {
+    }
 }
